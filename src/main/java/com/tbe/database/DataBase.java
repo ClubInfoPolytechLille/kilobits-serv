bea35f9f094e6cbebc84c0deb6ced772b91f0927
badetitou <badetitou@gmail.com>
2016-12-02 03:24:04 +0100

    Some corection

--- src/main/java/com/tbe/database/DataBase.java
+++ src/main/java/com/tbe/database/DataBase.java
@@ -34,14 +34,14 @@ public class DataBase {
 			+ "Mdp varchar(30) NOT NULL DEFAULT '0000', "
 			+ "Ville varchar(30), "
 			+ "EstMobile BOOL NOT NULL DEFAULT 'false', "
-			+ "Type BOOL NOT NULL DEFAULT 'false', " // Par default t'es migrant en false
+			+ "Typ BOOL NOT NULL DEFAULT 'false', " // Par default t'es migrant en false
 			+ "Divers VARCHAR(30), "
 			+ "Dispo BOOL DEFAULT 'false');";
 
 	private static String strCreateEvenementTable = "Create table if not exists Evenement ("
 			+ "Id integer primary key autoincrement,"
 			+ "Lieu varchar(20) NOT NULL,"
-			+ "Date varchar(20) NOT NULL,"
+			+ "Dat varchar(20) NOT NULL,"
 			+ "Description varchar(500) NOT NULL);";
 
 	private static String strCreateBesoinTable = "Create table if not exists Besoin ("
@@ -95,7 +95,7 @@ public class DataBase {
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:"+System.getProperty("java.io.tmpdir")
-					+System.getProperty("file.separator")+"prolab.db");
+					+System.getProperty("file.separator")+"kilobits.db");
 			DataBase.c = c;
 			createTable();
 		} catch (Exception e) {
@@ -116,7 +116,8 @@ public class DataBase {
         stmt.executeUpdate(strCreateOrganiseTable);
         stmt.executeUpdate(strCreateUtilisateurTable);
         stmt.executeUpdate(strCreateLangueTable);
-        stmt.executeUpdate(strCreateCategorieTable);
+		System.out.println("done");
+		stmt.executeUpdate(strCreateCategorieTable);
         stmt.executeUpdate(strCreateEvenementTable);
         stmt.executeUpdate(strCreateParticipeTable);
         stmt.executeUpdate(strCreateBesoinTable);
