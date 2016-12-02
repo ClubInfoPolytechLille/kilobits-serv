5fcb3e7493ae2c1dd33c39b6af98a8cc123eb763
badetitou <badetitou@gmail.com>
2016-12-02 03:14:47 +0100

    Restart

--- src/main/java/com/tbe/json/Chat.java
+++
@@ -1,9 +0,0 @@
-package com.tbe.json;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class Chat {
-	static public Map<Integer,Salon> salons = new HashMap<Integer,Salon>();
-	public Chat(){}
-}
