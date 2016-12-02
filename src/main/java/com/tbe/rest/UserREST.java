04415665d1bac38b1de9b76576a42f87e04cd47b
badetitou <badetitou@gmail.com>
2016-12-02 03:49:25 +0100

    Add user POST

--- src/main/java/com/tbe/rest/UserREST.java
+++ src/main/java/com/tbe/rest/UserREST.java
@@ -1,61 +1,26 @@
 package com.tbe.rest;
 
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.sun.org.apache.regexp.internal.RE;
 import com.tbe.database.UsersRequest;
 import com.tbe.json.User;
 
 @Path("/users")
 public class UserREST {
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{username}&{userpassword}")
-	public User getUser(@PathParam("username") String username,
-			@PathParam("userpassword") String password) {
-		username = username.toLowerCase();
-		System.out.println("GET USER " + username + " : " + password);
-		return UsersRequest.getUser(username, password);
-	}
+    @POST
+    public Response createUser(User user){
+        System.out.println("Post User");
+        String result = UsersRequest.addUser(user);
+        if (result == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Entity already exist").build();
+        }
+        return Response.status(Response.Status.CREATED).entity("News Created")
+                .build();
+    }
 
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Path("/{username}")
-	public User getUser(@PathParam("username") String username) {
-		username = username.toLowerCase();
-		System.out.println("GET USER " + username);
-		return UsersRequest.getUser(username);
-	}
-
-	@PUT
-	@Path("/{username}")
-	public Response update(User user, @PathParam("username") String username) {
-		int i = UsersRequest.update(user, username);
-		if (i > 0) {
-			return Response.status(Response.Status.OK).entity("update").build();
-		} else {
-			return Response.status(Response.Status.BAD_REQUEST)
-					.entity("User already exist or no exists").build();
-		}
-
-	}
-
-	@DELETE
-	@Path("/{username}")
-	public Response delete(@PathParam("username") String username) {
-		int i = UsersRequest.delete(username);
-		if (i > 0) {
-			return Response.status(Response.Status.OK).entity("delete").build();
-		} else {
-			return Response.status(Response.Status.BAD_REQUEST)
-					.entity("can't delete users : " + username).build();
-		}
-	}
 }
